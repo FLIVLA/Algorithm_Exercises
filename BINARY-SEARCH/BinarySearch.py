4e894d319d0b0ab048764742533ce4882be67bb1
Frederik Lind <fred2193@stud.kea.dk>
2023-06-06 09:30:36 +0200

    binary search examples

---
+++ BINARY-SEARCH/BinarySearch.py
@@ -0,0 +1,12 @@
+class BinarySearch(object):
+    
+    def search_PosOf_t(self, nums, t):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + (r - l + 1) // 2
+            if nums[m] < t:
+                l = m + 1
+            elif nums[m] > t:
+                r = m - 1
+            else: return m
+        return l
---
+++ BINARY-SEARCH/BinarySearch.ts
@@ -0,0 +1,20 @@
+class BinarySearch {
+
+    /**
+     * BINARY SEARCH FOR SORTED NUMBER[]
+     * @param nums input sorted number array
+     * @param t target value
+     * @returns index of t in nums
+     */
+    public search_PosOf_t(nums: number[], t: number): number {
+        var l = 0, r = nums.length - 1;
+        while (l <= r) {
+            var m = Math.floor((l + r) / 2);
+            if (nums[m] < t) {
+                l = m + 1;
+            } else if (nums[m] > t) {
+                r = m - 1;
+            } else return m;
+        } return l;
+    }
+}
